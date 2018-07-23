e15110e622276bffeb984adbd5e60b52e4f85468
Timothey Mahieux <timothey.mahieux@gmail.com>
2018-07-23 12:37:02 +0200

    Création class Equipe

---
+++ app/src/main/java/fr/handstbrice/handballstbrice/model/Equipe.java
@@ -0,0 +1,12 @@
+package fr.handstbrice.handballstbrice.model;
+
+public class Equipe {
+
+    private int id;
+    private String nom;
+
+    public Equipe(int id, String nom){
+        this.id = id;
+        this.nom = nom;
+    }
+}
