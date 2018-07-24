35ed35b7f52201edfe08441a6824c0b0fc869e11
valensto <v.e.brochard@gmail.com>
2018-07-24 16:37:26 +0200

    ajout d'url

--- app/src/main/java/fr/handstbrice/handballstbrice/AccueilActivite.java
+++ app/src/main/java/fr/handstbrice/handballstbrice/AccueilActivite.java
@@ -3,13 +3,27 @@ package fr.handstbrice.handballstbrice;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import fr.handstbrice.handballstbrice.model.Equipe;
+import fr.handstbrice.handballstbrice.model.Match;
+import fr.handstbrice.handballstbrice.rss.FluxRSS;
 
 public class AccueilActivite extends AppCompatActivity {
     Intent mServiceIntent;
     private MatchCheckerService matchCheckerService;
+    private CircleImageView tvEquipeGauche, tvEquipeDroite;
+    private TextView tvScoreEquipeGauche, tvScoreEquipeDroite;
+    private List<Equipe> equipes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +33,44 @@ public class AccueilActivite extends AppCompatActivity {
         if (!isMyServiceRunning(matchCheckerService.getClass())) {
             startService(mServiceIntent);
         }
+
+        update();
+
+    }
+
+    private void update()
+    {
+
+        try {
+            equipes=FluxRSS.scanListEquipes().execute().get();
+            List<Match> lastMatchs = FluxRSS.scanLastMatchs().execute().get();
+            if (lastMatchs.size()>0) {
+                Match m=lastMatchs.get(0);
+
+                Equipe ed=getEquipeByName(m.getEquipeExterieure());
+                tvEquipeDroite = (CircleImageView) findViewById(R.id.equipe_droite);
+                if (ed!=null && ed.getSrcImg()!=null)
+                    tvEquipeGauche.setImageURI(ed.getSrcImg());
+
+                Equipe eg=getEquipeByName(m.getEquipeLocale());
+                tvEquipeGauche = (CircleImageView) findViewById(R.id.equipe_gauche);
+                if (eg!=null && eg.getSrcImg()!=null)
+                    tvEquipeGauche.setImageURI(eg.getSrcImg());
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Equipe getEquipeByName(String name)
+    {
+        for (Equipe e : equipes)
+            if (e.getNom().equals(name))
+                return e;
+        return null;
+
     }
 
     private boolean isMyServiceRunning(Class<?> serviceClass) {
