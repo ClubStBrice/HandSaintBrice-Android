3b59ffb3c5a90fca9ba4ae980a93ecac6c4a61cc
valensto <v.e.brochard@gmail.com>
2018-07-24 11:45:25 +0200

    ajout d'url

--- app/src/main/java/fr/handstbrice/handballstbrice/model/Equipe.java
+++ app/src/main/java/fr/handstbrice/handballstbrice/model/Equipe.java
@@ -1,5 +1,9 @@
 package fr.handstbrice.handballstbrice.model;
 
+import android.net.Uri;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,12 +11,15 @@ public class Equipe {
 
     private int id;
     private String nom;
+    private Uri srcImg;
     private List<Joueur> joueurs=new ArrayList<>();
 
-    public Equipe(int id, String nom){
+    public Equipe(int id, String nom, String srcImg){
         assert nom != null;
         this.id = id;
         this.nom = nom;
+        this.srcImg=Uri.parse(srcImg);
+
     }
 
     void addJoueur(Joueur j){
@@ -32,5 +39,9 @@ public class Equipe {
 
     public List<Joueur> getJoueurs() {
         return joueurs;
+    }
+
+    public Uri getSrcImg() {
+        return srcImg;
     }
 }
