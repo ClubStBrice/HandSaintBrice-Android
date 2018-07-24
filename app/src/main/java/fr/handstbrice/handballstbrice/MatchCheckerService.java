e166f49d2e6efbb7241506a7363032b874c2e065
PaulineM0331 <montreuil.p@outloook.com>
2018-07-24 10:25:18 +0200

    Ajout service vérification match

--- app/src/main/java/fr/handstbrice/handballstbrice/MatchCheckerService.java
+++ app/src/main/java/fr/handstbrice/handballstbrice/MatchCheckerService.java
@@ -2,10 +2,14 @@ package fr.handstbrice.handballstbrice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
 
 import fr.handstbrice.handballstbrice.model.Match;
@@ -26,12 +30,30 @@ public class MatchCheckerService extends Service {
                 //TODO à compléter
                 try {
                     Match match=FluxRSS.scanLastMatch().execute().get();
+                    SharedPreferences bdd = getBaseContext().getSharedPreferences("BDD", MODE_PRIVATE);
+                    long dateDernierMatchUTC=bdd.getLong("dateDernierMatcthUTC", 0);
+
+                    if (match!=null && (dateDernierMatchUTC==0 || dateDernierMatchUTC<match.getDateUTC())
+                    {
+                        //notification
+                        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+//                            .setSmallIcon(R.drawable.notification_icon)
+                            .setContentTitle("Handball St Brice Courcelles")
+                            .setContentText("Un match aura lieu le ")
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                        //maj date
+                        bdd.edit().putLong("dateDernierMatcthUTC", match.getDateUTC()).commit();
+                    }
+
 
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } catch (ExecutionException e) {
                     e.printStackTrace();
                 }
+
+
                 handler.postDelayed(this, 30L*60L*1000L);
             }
         };
