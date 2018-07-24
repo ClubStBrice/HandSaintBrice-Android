0a4c0be620a58c87d2c512d4372303e4313c329e
izidasians <izida51100@gmail.com>
2018-07-24 16:40:43 +0200

    Premier test

--- app/src/main/java/fr/handstbrice/handballstbrice/DernierMatchActivite.java
+++ app/src/main/java/fr/handstbrice/handballstbrice/DernierMatchActivite.java
@@ -3,11 +3,36 @@ package fr.handstbrice.handballstbrice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import fr.handstbrice.handballstbrice.model.Match;
+import fr.handstbrice.handballstbrice.rss.FluxRSS;
+
 public class DernierMatchActivite extends AppCompatActivity {
+
+    private Match match;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dernier_match_activite);
+        List<Match>l= null;
+        try {
+            l = FluxRSS.scanLastMatchs().execute().get();
+            if (l.size() > 0) {
+                match = l.get(0);
+
+
+
+
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
