b0db1d818a1fa154441ebbb17d8aeb8bc23126e3
QuasarU1.27 <hanwei51@hotmail.com>
2018-07-24 15:28:28 +0200

    Scan des joueurs

--- app/src/main/java/fr/handstbrice/handballstbrice/rss/FluxRSS.java
+++ app/src/main/java/fr/handstbrice/handballstbrice/rss/FluxRSS.java
@@ -21,6 +21,7 @@ import java.util.List;
 
 import fr.handstbrice.handballstbrice.model.Article;
 import fr.handstbrice.handballstbrice.model.Equipe;
+import fr.handstbrice.handballstbrice.model.Joueur;
 import fr.handstbrice.handballstbrice.model.Match;
 import fr.handstbrice.handballstbrice.model.Partenaire;
 
@@ -91,6 +92,11 @@ public class FluxRSS
                     //le flux de données inputSource
                     xmlReader.parse(inputSource);
 
+
+
+                    scanListJoueurs(rssHandler.getRssResult());
+
+
                     //on retourne le résultat
                     return rssHandler.getRssResult();
 
@@ -101,6 +107,39 @@ public class FluxRSS
             }
         };
     }
+
+    private static List<Joueur> scanListJoueurs(List<Equipe> equipesList)
+    {
+
+        try {
+            //à partir de cette URL
+            URL rssUrl = new URL("http://dev-handstbrice.fr/api-stbrice/?action=get_list_joueurs");
+            //on construit un parseur (parcoureur) de contenu XML
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            XMLReader xmlReader = saxParser.getXMLReader();
+
+            //grâce à notre lecteur de contenu de RSS:
+            JoueurRSSHandler rssHandler = new JoueurRSSHandler(equipesList);
+            //on informe notre parser quelle classe il devra utiliser lors de la lecture
+            //de chaque élément XML
+            xmlReader.setContentHandler(rssHandler);
+            //on cré un flux de données à partir de l'URL définie plus haut
+            InputSource inputSource = new InputSource(rssUrl.openStream());
+
+            //on demande à notre parser de parcourir tout le contenu XML renvoyé par
+            //le flux de données inputSource
+            xmlReader.parse(inputSource);
+
+            //on retourne le résultat
+            return rssHandler.getRssResult();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
 
     public static AsyncTask<String,Void, List<Article>> scanArticles()
     {
