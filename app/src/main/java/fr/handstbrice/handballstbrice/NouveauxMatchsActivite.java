a9cd5dd3a946aaa95c22e7dc3b0e5f83ebe0edb8
Julia <clystere08@hotmail.fr>
2018-07-24 16:55:31 +0200

    Ajout de l'activité permettant d'afficher les prochains matchs

---
+++ app/src/main/java/fr/handstbrice/handballstbrice/NouveauxMatchsActivite.java
@@ -0,0 +1,13 @@
+package fr.handstbrice.handballstbrice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NouveauxMatchsActivite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nouveaux_matchs_activite);
+    }
+}
