2980c7978dcb19394a5f21ce9b4bad57164667f8
Julia <clystere08@hotmail.fr>
2018-07-25 10:30:24 +0200

    ajoute activité articles

---
+++ app/src/main/java/fr/handstbrice/handballstbrice/ArticlesActivite.java
@@ -0,0 +1,13 @@
+package fr.handstbrice.handballstbrice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArticlesActivite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_articles_activite);
+    }
+}
